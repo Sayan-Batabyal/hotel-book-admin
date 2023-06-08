b1880b223067cd30555a3ed5349a9a5d847c8607
Sayan Batabyal <sayanbatabyal.mav@gmail.com>
2023-06-08 23:02:05 +0530

    new deploy

--- api/index.js
+++ api/index.js
@@ -4,7 +4,6 @@ import mongoose from 'mongoose';
 import userRoute from './routes/users.js'
 import roomsRoute from './routes/rooms.js'
 
-
 const app=express()
 dotenv.config()
 
@@ -21,15 +20,19 @@ try {
 app.use(express.json())
 
 app.use("/api/user",userRoute)
-
 app.use("/api/room",roomsRoute)
 
+
 app.get("/",(req,res)=>{
-    res.send("H")
+    res.send("Welcome to my Hotel Api")
 })
 
 
+
+connect().then(()=>{
 app.listen(8800,()=>{
-    connect()
     console.log("Connected to Backend");
+})})
+.catch((err)=>{
+    console.log(err);
 })
